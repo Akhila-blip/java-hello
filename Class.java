ec9cc05c238a2529df73cb9d7dc138d9342023a1
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-06-05 16:23:54 -0500

    Added Class program

---
+++ Class.java
@@ -0,0 +1,17 @@
+Class:
+
+*. A Class Is A Template For Creating Objects,Whic Defines Its State And Behaviuor
+
+*A Class Contains Fields And Methods To Define The State And Behaviour Of Its Object
+
+*.Fields Are Used To  Store The Data
+
+*.Methods Are Used To Perform Some Operations
+
+*.Syntax:
+  class<Class-Name>extends
+   
+  class A
+  {
+   int x=5;
+  }
